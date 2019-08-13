2012d0635895da8ceed1720a22aff0b371cc6f6d
Ben Sevcik <43259346+bsevcik@users.noreply.github.com>
2019-08-13 16:36:42 -0700

    took out styling from blank-box7

---
+++ src/components/Stylesheet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css';
+function stylesheet() {
+    return (
+        <div>
+            <h1>stylesheets</h1>
+        </div>
+    )
+}
+
+export default stylesheet
